e807537c0e5a5cabd6c424a91de8b40d42c11286
Steven <houbo.ye@insolu.cn>
2026-06-07 09:38:31 +0800

    feat:init

---
+++ src/main/java/com/bc/app_deploy/AppDeployApplication.java
@@ -0,0 +1,13 @@
+package com.bc.app_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDeployApplication.class, args);
+    }
+
+}
