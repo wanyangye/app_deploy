de06f7a478d0984eca964bc3cf6cfea2eab3e6b3
Steven <houbo.ye@insolu.cn>
2026-06-07 10:20:55 +0800

    feat:init

--- src/main/java/com/bc/app_deploy/AppDeployApplication.java
+++ src/main/java/com/bc/app_deploy/AppDeployApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppDeployApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(AppDeployApplication.class, args);
     }
---
+++ src/main/java/com/bc/app_deploy/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.bc.app_deploy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
+
